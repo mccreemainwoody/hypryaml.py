5758d07100a9468d0e1daa885f30b10d08b7cd52
Clément METZ <metz.clement@gmail.com>
2025-07-16 19:57:07 +0200

    feat(utils/hyprpaper): implemented utils function to communicate with the hyprpaper API
    created testsuite for the utils.hyprpaper.run_hyprpaper_command function
    
    testsuite files now always have unique names

--- src/utils/hyprpaper/run_commads.py
+++ src/utils/hyprpaper/run_commads.py
@@ -1,14 +1,68 @@
-from models.CommandResult import CommandResult
+from models import CommandResult
 from ..hyprctl import run_hyprctl_command
 
+from collections.abc import Collection
+from os import PathLike
 
-def run_hyprpaper_command(command: str) -> CommandResult:
+
+def run_hyprpaper_command(args: Collection[str,]) -> CommandResult:
     """Run a hyprpaper command and return its output.
 
     Args:
-        command (str): The hyprpaper command to run.
+        args (Collection[str]): The hyprpaper command to run.
 
     Returns:
         CommandResult: The output of the command.
     """
-    return run_hyprctl_command(f"hyprpaper {command}")
+    command_args = ["hyprpaper", *args]
+    return run_hyprctl_command(command_args)
+
+
+def run_hyprpaper_preload(path: PathLike) -> CommandResult:
+    """Preload a wallpaper of the hyprpaper API.
+
+    Args:
+        path (PathLike): The path to the wallpaper image.
+
+    Returns:
+        CommandResult: The output of the command.
+    """
+    commands_args = ["preload", str(path)]
+    return run_hyprpaper_command(commands_args)
+
+
+def run_hyprpaper_unload(path: PathLike) -> CommandResult:
+    """Unload a wallpaper of the hyprpaper API.
+
+    Args:
+        path (PathLike): The path to the wallpaper image.
+
+    Returns:
+        CommandResult: The output of the command.
+    """
+    commands_args = ["unload", str(path)]
+    return run_hyprpaper_command(commands_args)
+
+
+def run_hyprpaper_unload_all() -> CommandResult:
+    """Unload all the wallpapers of the hyprpaper API.
+
+    Returns:
+        CommandResult: The output of the command.
+    """
+    commands_args = ["unload", "all"]
+    return run_hyprpaper_command(commands_args)
+
+
+def run_hyprpaper_wallpaper(monitor: str, path: PathLike) -> CommandResult:
+    """Set a wallpaper for a specific monitor using the hyprpaper API.
+
+    Args:
+        monitor (str): The monitor identifier to set the wallpaper of.
+        path (PathLike): The path to the wallpaper image.
+
+    Returns:
+        CommandResult: The output of the command.
+    """
+    commands_args = ["wallpaper", f"{monitor},{path}"]
+    return run_hyprpaper_command(commands_args)
