bc7114bddfc27c8d37b29be4937819bd69874638
Clément METZ <metz.clement@gmail.com>
2025-07-16 22:31:46 +0200

    feat(workflow): implemented application frontend
    added typer-slim to project dependencies

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-def main():  # noqa: D103
-    print("Hello from hyprthemes!")
+import typer
+
+from workflow import apply_configuration
+
+from pathlib import Path
+from typing import Annotated
+
+
+def main(
+        config: Annotated[
+            Path,
+            typer.Argument(
+                help="Path of the configuration file to apply. Must be a "
+                     "valid YAML file",
+                exists=True,
+                file_okay=True,
+                dir_okay=False,
+                readable=True,
+                resolve_path=True
+            )
+        ]
+):  # noqa: D103
+    success = apply_configuration(config)
+
+    if success:
+        typer.echo("Configuration applied successfully.")
+    else:
+        typer.echo("Failed to apply configuration.", err=True)
+        raise typer.Exit(code=1)
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
