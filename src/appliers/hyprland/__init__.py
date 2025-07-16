ea27a1555712528cbe9e4bfad89e74df1bf290c3
Clément METZ <metz.clement@gmail.com>
2025-07-16 22:13:39 +0200

    feat(appliers/hyprland): implemented apply_configuration methods, which applies a YAML configuration to the current Hyprland session

--- src/appliers/hyprland/__init__.py
+++ src/appliers/hyprland/__init__.py
@@ -1,7 +1,9 @@
 """Functions used to apply Hyprland configurations."""
 
+from .apply_configuration import apply_hyprland_configuration
 from .load_config import generate_keywords
 
 __all__ = [
+    "apply_hyprland_configuration",
     "generate_keywords",
 ]
